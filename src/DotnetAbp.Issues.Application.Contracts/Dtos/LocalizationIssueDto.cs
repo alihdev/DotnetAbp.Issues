2516be8b64c2c27d1e3966fe17b8cbcfd057440a
Ali Hassan <97635301+alihdev@users.noreply.github.com>
2023-11-01 12:57:32 +0300

    Add LocalizationIssueAppService for 400 issue with ar/-1

---
+++ src/DotnetAbp.Issues.Application.Contracts/Dtos/LocalizationIssueDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DotnetAbp.Issues.Dtos
+{
+    public class LocalizationIssueDto
+    {
+        public int Factor { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ src/DotnetAbp.Issues.Application/Issues/LocalizationIssueAppService.cs
@@ -0,0 +1,12 @@
+﻿using DotnetAbp.Issues.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAbp.Issues.Issues;
+
+public class LocalizationIssueAppService : IssuesAppService
+{
+    public LocalizationIssueDto CreateAsync([FromForm] LocalizationIssueDto input)
+    {
+        return input;
+    }
+}
