73f5ec5602724130651306c3040c6531ecea64ed
Ali Hassan <97635301+alihdev@users.noreply.github.com>
2023-11-02 13:14:08 +0300

    Add TestArCulture

--- src/DotnetAbp.Issues.Application/Issues/LocalizationIssueAppService.cs
+++ src/DotnetAbp.Issues.Application/Issues/LocalizationIssueAppService.cs
@@ -1,5 +1,8 @@
 ﻿using DotnetAbp.Issues.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel;
+using System.Globalization;
 
 namespace DotnetAbp.Issues.Issues;
 
@@ -8,5 +11,41 @@ public class LocalizationIssueAppService : IssuesAppService
     public LocalizationIssueDto CreateAsync([FromForm] LocalizationIssueDto input)
     {
         return input;
+    }
+
+    public void TestArCulture()
+    {
+        var typeDescriptor = TypeDescriptor.GetConverter(typeof(decimal));
+
+        var enCultureInfo = new CultureInfo("en");
+
+        var resultEn1 = typeDescriptor.ConvertFrom(null, enCultureInfo, "-1"); // -1
+        var resultEn2 = typeDescriptor.ConvertFrom(null, enCultureInfo, "1.25"); // 1.25
+
+        var arCultureInfo = new CultureInfo("ar");
+
+        try
+        {
+            var resultAr = typeDescriptor.ConvertFrom(null, arCultureInfo, "-1"); // System.ArgumentException: '-1 is not a valid value for Decimal. Arg_ParamName_Name'
+        }
+        catch (Exception ex)
+        {
+        }
+
+        try
+        {
+            var resultAr = typeDescriptor.ConvertFrom(null, arCultureInfo, "1-"); // System.ArgumentException: '1- is not a valid value for Decimal. Arg_ParamName_Name'
+        }
+        catch (Exception ex)
+        {
+        }
+
+        try
+        {
+            var resultAr = typeDescriptor.ConvertFrom(null, arCultureInfo, "1.25"); // System.ArgumentException: '1.25 is not a valid value for Decimal. Arg_ParamName_Name'
+        }
+        catch (Exception ex)
+        {
+        }
     }
 }
