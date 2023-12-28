26abe5e38fe60d15b8a6ffc861464b1fee59fb38
alihdev <ahdev12r@gmail.com>
2023-12-28 09:43:51 +0300

    Inject IDistributedCache to clear permissions cache

--- services/cmskit/src/EShopOnAbp.CmskitService.Application/Samples/SampleAppService.cs
+++ services/cmskit/src/EShopOnAbp.CmskitService.Application/Samples/SampleAppService.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Options;
 using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.Caching;
 using Volo.Abp.PermissionManagement;
 
 namespace EShopOnAbp.CmskitService.Samples;
@@ -11,12 +14,23 @@ public class SampleAppService : CmskitServiceAppService, ISampleAppService
     private readonly IPermissionDefinitionRecordRepository _permissionDefinitionRecordRepository;
     private readonly IPermissionManager _permissionManager;
     private readonly IDynamicPermissionDefinitionStoreInMemoryCache _storeCache;
+    private readonly IDistributedCache _distributedCache;
+    private AbpDistributedCacheOptions CacheOptions { get; }
 
-    public SampleAppService(IPermissionManager permissionManager, IDynamicPermissionDefinitionStoreInMemoryCache storeCache, IPermissionDefinitionRecordRepository permissionDefinitionRecordRepository)
+
+    public SampleAppService(
+        IPermissionManager permissionManager, 
+        IDynamicPermissionDefinitionStoreInMemoryCache storeCache, 
+        IPermissionDefinitionRecordRepository permissionDefinitionRecordRepository, 
+        IDistributedCache distributedCache,
+        IOptions<AbpDistributedCacheOptions> cacheOptions
+    )
     {
         _permissionManager = permissionManager;
         _storeCache = storeCache;
         _permissionDefinitionRecordRepository = permissionDefinitionRecordRepository;
+        _distributedCache = distributedCache;
+        CacheOptions = cacheOptions.Value;
     }
 
     public Task<SampleDto> GetAsync()
@@ -56,6 +70,7 @@ public class SampleAppService : CmskitServiceAppService, ISampleAppService
         // clear cache
         _storeCache.LastCheckTime = null;
         _storeCache.CacheStamp = null;
+        await _distributedCache.RemoveAsync($"{CacheOptions.KeyPrefix}_AbpInMemoryPermissionCacheStamp");
 
         // assing admin to permission
         await _permissionManager.SetAsync(permissionCreateDto.Name, RolePermissionValueProvider.ProviderName, "admin", true);
